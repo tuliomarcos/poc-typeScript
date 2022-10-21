8c62c45dbe3a278f1281b0780ae4fa5378acb218
tuliosilva.iblue <tuliosilva.iblue@ciclic.com.br>
2022-10-21 19:34:22 -0300

    feature: adcionando tipagem para o arquivo service

--- src/http.ts
+++ src/http.ts
@@ -1,9 +1,29 @@
-const request = (url, options) =>
+type DataRequest = {
+  image: string,
+  brandModel: string,
+  year: number,
+  plate: string,
+  color: string,
+}
+
+type ValidHeaders = 'content-type' | 'accept'
+
+type HeadersRquest = {
+  [key in ValidHeaders]?: string
+}
+
+type OptionsRequest = ({
+  method: string
+  headers: HeadersRquest,
+  body: string
+})
+
+const request = (url: string, options?: OptionsRequest) =>
   fetch(url, options)
     .then(r => r.json())
     .catch(e => ({ error: true, message: e.message }))
 
-const createRequest = (method) => (url, data) => request(url, {
+const createRequest = (method: string) => (url: string, data: DataRequest) => request(url, {
   method,
   headers: {
     'content-type': 'application/json',
@@ -11,6 +31,6 @@ const createRequest = (method) => (url, data) => request(url, {
   body: JSON.stringify(data)
 })
 
-export const get = (url) => request(url)
+export const get = (url: string) => request(url)
 export const post = createRequest('POST')
 export const del = createRequest('DELETE')
